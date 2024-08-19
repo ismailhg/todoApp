93537deab8b1e9ad24db89e340a552dba9854c99
ismailhg <133405153+ismailhg@users.noreply.github.com>
2024-08-19 16:19:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -314,11 +314,3 @@ function showNotification(message) {
         notification.style.display = 'none';
     }, 2000);
 }
-
-function getTime() {
-    let time = new Date();
-    let day = String(time.getDate()).padStart(2, '0');
-    let month = String(time.getMonth() + 1).padStart(2, '0');
-    let year = time.getFullYear();
-    return `${day}.${month}.${year}`;
-}
